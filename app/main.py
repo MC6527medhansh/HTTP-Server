898c460de497f49680fbde6ed173a797dbc9b50c
Medhansh Choubey <medhanshchoubey619@gmail.com>
2024-11-24 23:54:01 -0800

    created, bound and made the socket listen on port & Add URL path handling for HTTP requests with 200 and 404 responses - Implement parsing of HTTP request to extract method, path, and version from the request line. - Respond with "200 OK" for requests to `/index.html`. - Respond with "404 Not Found" for all other paths. - Handle malformed requests by responding with "400 Bad Request". - Added debug logs to print method, path, and version for incoming requests.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import socket  # noqa: F401
 
+
 def main():
     print("Server is starting...")
 
-    # Create a server socket that would listen on localhost 4221
+    # Create a server socket that binds to the address specified and
+    # would listen on localhost 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server is listening on port 4221...")
 
@@ -11,14 +13,33 @@ def main():
         # Wait for a client to connect
         client_socket, client_address = server_socket.accept()
         print(f"Connection established with {client_address}")
-
-        # Send the HTTP 200 OK response
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+        
+        # Recieve data from the client socket => get client request
+        client_request = client_socket.recv(1024).decode()  # decode to get string data from bytes
+        
+        # Extract URL => Get client request line
+        try:
+            client_request_line = client_request.split("\r\n")[0]  # split string into list to pick individual parts
+            method, path, version = client_request_line.split(" ")
+            print(f"Method: {method}, Path: {path}, Version: {version}")
+        except ValueError:
+            # If the request is malformed then respond with 400 Bad Request
+            response = "HTTP/1.1 400 Bad Request\r\n\r\n"
+            client_socket.sendall(response.encode())
+            client_socket.close()
+            continue
+            
+        if path == "/index.html":
+            # Send the HTTP 200 OK response
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            # Respond with 404 Not Found
+            response = "HTTP/1.1 404 Not Found\r\n\r\nThe requested resource was not found."
+            
         client_socket.sendall(response.encode())  # Send the response
 
         # Close the connection
         client_socket.close()
-
 
 
 if __name__ == "__main__":
