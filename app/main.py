3c7ffb7935221d9169b2ec906d52d7da9d7cd19d
Medhansh Choubey <medhanshchoubey619@gmail.com>
2024-11-25 23:13:59 -0800

    extracted header line and dealt with user agent request

--- app/main.py
+++ app/main.py
@@ -19,8 +19,9 @@ def main():
         
         # Extract URL => Get client request line
         try:
-            client_request_line = client_request.split("\r\n")[0]  # split string into list to pick individual parts
-            method, path, version = client_request_line.split(" ")
+            client_request_lines = client_request.split("\r\n")  # split string into list to pick individual parts
+            client_request_line = client_request[0]  # first line contains request line from where 
+            method, path, version = client_request_line.split(" ")  # you extract the method, path and version
             print(f"Method: {method}, Path: {path}, Version: {version}")
         except ValueError:
             # If the request is malformed then respond with 400 Bad Request
@@ -29,6 +30,13 @@ def main():
             client_socket.close()
             continue
             
+        # Extract the header line
+        headers = [] 
+        for line in client_request_lines[1:]:  # Start from the second line and skip the request line
+            if ":" in line:  # Check if the line contains a header in 'Key: Value' format
+                key, value = line.split(": ", 1)  # Split the line at most 1 time by using ": "
+                headers[key.lower()] = value  # Store the header in lowercase for case insensitivity
+        
         if path == "/index.html":
             # Send the HTTP 200 OK response
             response = "HTTP/1.1 200 OK\r\n\r\n"
@@ -40,6 +48,14 @@ def main():
                 f"Content-Type: text/plain\r\n"
                 f"Content-Length: {len(echo_string)}\r\n\r\n"
                 f"{echo_string}"
+            )
+        elif path == "/user-agent":
+            user_agent = headers.get("user-agent", "")
+            response = (
+                f"HTTP/1.1 200 OK\r\n"
+                f"Content-Type: text/plain\r\n"
+                f"Content-Length: {len(user_agent)}\r\n\r\n"
+                f"{user_agent}"
             )
         else:
             # Respond with 404 Not Found
